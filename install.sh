308873e8b852d56c99fea051374a664b8b2dad6b
SJHong <sjhong310@contec.kr>
2021-04-16 14:06:28 +0900

    LabelImg install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install pyqt5-dev-tools
+pip install -r requirements/requirements-linux-python3.txt
+make qt5py3
+
+# In case you get error"Could not load the Qt platform plugin 'xcb' in '' even though it was found"
+# sudo apt-get install libxcb-xinerama0
