6cccc5e23590804988017311f27541e79872c824
SJHong <sjhong310@contec.kr>
2021-04-20 15:03:14 +0900

    Add labelImg path to env path

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@ sudo apt-get install pyqt5-dev-tools
 pip install -r requirements/requirements-linux-python3.txt
 make qt5py3
 
-# In case you get error"Could not load the Qt platform plugin 'xcb' in '' even though it was found"
+PROJECT_DIR=$(pwd)
+sudo echo export PATH_labelImg=\"/home/sjhong/work/labelImg\" >> ~/.bashrc
+sudo echo export PATH="$"{PATH}":/usr/local/cuda-10.0/bin:$"{PATH_labelImg}"" >> ~/.bashrc
+source ~/.bashrc
+
+# In case you get error "Could not load the Qt platform plugin 'xcb' in '' even though it was found"
 # sudo apt-get install libxcb-xinerama0
