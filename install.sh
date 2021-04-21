cc2029e965d04db577b0b491a45f6068b80a08d3
SJHong <sjhong310@contec.kr>
2021-04-21 11:40:43 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ pip install -r requirements/requirements-linux-python3.txt
 make qt5py3
 
 PROJECT_DIR=$(pwd)
-sudo echo export PATH_labelImg=\"/home/sjhong/work/labelImg\" >> ~/.bashrc
-sudo echo export PATH="$"{PATH}":/usr/local/cuda-10.0/bin:$"{PATH_labelImg}"" >> ~/.bashrc
+echo export PATH_labelImg=\"${PROJECT_DIR}/work/labelImg\" >> ~/.bashrc
+echo export PATH="$"{PATH}":$"{PATH_labelImg}"" >> ~/.bashrc
 source ~/.bashrc
 
 # In case you get error "Could not load the Qt platform plugin 'xcb' in '' even though it was found"
